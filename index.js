3f13e377b4beea40611ac5cf8cc2bb9528733302
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-12-26 18:32:24 -0500

    Summary

---
+++ index.js
@@ -0,0 +1,13 @@
+const text = "I am a web developer...";
+let i = 0;
+let speed = 100;
+
+function type() {
+  if (i < text.length) {
+    document.querySelector(".copiration").textContent += text.charAt(i);
+    i++
+    setTimeout(type, speed);
+  }
+}
+type()
+
